bc9c23ae5f7c9da4a8050db73141c1720fb7b1a4
Aiman03at <aiman03at@gmail.com>
2024-06-10 13:17:19 -0400

    Setup stdin to send keyboard command from players

--- play.js
+++ play.js
@@ -1,5 +1,24 @@
 /////a function called connect which sets up a connection and returns an object.
 const connect=require("./client")
 console.log("Connecting ...");
-connect();
 
+
+// setup interface to handle user input from stdin
+//the stdin object returned by setupInput will allow us to listen for keyboard input and react to it
+const setupInput = function () {
+  const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
+  stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
+  stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
+  stdin.resume(); // resume stdin so the program can listen for input
+  stdin.on("data", (key) => handleUserInput(key));//what happens when "data" is received from stdin..
+  return stdin;   // return the stdin object so we can use it elsewhere in the program
+};
+
+const handleUserInput = function (key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+connect();
+setupInput();
