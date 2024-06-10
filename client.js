bed87d003d1d235dc79364c57005c0f8bed10652
Aiman03at <aiman03at@gmail.com>
2024-06-10 11:09:29 -0400

    Console a message on succesfull connection and Send a name to the server

--- client.js
+++ client.js
@@ -1,13 +1,27 @@
 // establishes a connection with the game server
+
 const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
     host: "localhost",// IP address here,
     port: 50541// PORT number here,
   });
+   
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  // prompting when connection is established
+  conn.on("connect", () => {
+    console.log("Connection successfully established");
+    conn.write("Name: MAS");
+  });
+
+  
+
+
+
+  //Recieving data from server
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
--- play.js
+++ play.js
@@ -1,8 +1,5 @@
 /////a function called connect which sets up a connection and returns an object.
 const connect=require("./client")
-
-
-
 console.log("Connecting ...");
 connect();
 
