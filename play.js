03e7196780eef3f9f63780627f06b60250382d75
Aiman03at <aiman03at@gmail.com>
2024-06-10 10:46:29 -0400

    Setting up the connection module

--- play.js
+++ play.js
@@ -1,21 +1,8 @@
 /////a function called connect which sets up a connection and returns an object.
+const connect=require("./client")
 
-const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",// IP address here,
-    port: 50541// PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-  return conn;
-};
 console.log("Connecting ...");
 connect();
 
